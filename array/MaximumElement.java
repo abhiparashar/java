c6c8f39846bbf4decb0a7cd233066a3da3f160cf
Abhishek Parashar <aparashar103@gmail.com>
2024-10-10 23:53:59 +0530

    maxelem

--- array/MaximumElement.java
+++ array/MaximumElement.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;;
+import java.util.Scanner;
 
-public class TakingInputVal {
+public class MaximumElement {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
         }
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            if (max < arr[i]) {
+                max = arr[i];
+            }
         }
+        System.out.println("The maximum element is: " + max);
         scn.close();
     }
 }
--- array/SumOfElements.java
+++ array/SumOfElements.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;;
+import java.util.Scanner;
 
-public class TakingInputVal {
+public class SumOfElements {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int i = 0; i <= n - 1; i++) {
             arr[i] = scn.nextInt();
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            sum = sum + arr[i];
         }
+        System.out.println(sum);
         scn.close();
     }
 }
