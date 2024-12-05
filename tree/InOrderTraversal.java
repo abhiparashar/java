119582654847f874b8fc0999efb6d4e46b94ff38
Abhishek Parashar <abhishek.parashar@nium.com>
2024-12-05 15:33:50 +0530

    inorder and postorder traversal

---
+++ tree/InOrderTraversal.java
@@ -0,0 +1,41 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+// InOrderTraversal is basically Node
+public class InOrderTraversal {
+  int val;
+  InOrderTraversal left;
+  InOrderTraversal right;
+
+  InOrderTraversal() {
+  }
+
+  InOrderTraversal(int val) {
+    this.val = val;
+  }
+
+  InOrderTraversal(int val, InOrderTraversal left, InOrderTraversal right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public List<Integer> inorderTraversal(InOrderTraversal root) {
+    List<Integer> list = new ArrayList<>();
+    inorderHelper(root, list);
+    return list;
+  }
+
+  public static void inorderHelper(InOrderTraversal root, List<Integer> list) {
+    if (root == null)
+      return;
+    inorderHelper(root.left, list);
+    list.add(root.val);
+    inorderHelper(root.right, list);
+  }
+}
---
+++ tree/PostOrderTraversal.java
@@ -0,0 +1,41 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.*
+//PostOrderTraversal is Node basically
+public class PostOrderTraversal {
+  int val;
+  PostOrderTraversal left;
+  PostOrderTraversal right;
+
+  PostOrderTraversal() {
+  }
+
+  PostOrderTraversal(int val) {
+    this.val = val;
+  }
+
+  PostOrderTraversal(int val, PostOrderTraversal left, PostOrderTraversal right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public List<Integer> postorderTraversal(PostOrderTraversal root) {
+    List<Integer> list = new ArrayList<>();
+    postHelper(root, list);
+    return list;
+  }
+
+  public static void postHelper(PostOrderTraversal root, List<Integer> list) {
+    if (root == null)
+      return;
+    postHelper(root.left, list);
+    postHelper(root.right, list);
+    list.add(root.val);
+  }
+}
