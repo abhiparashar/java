3250fd71daffda236dba25c60ccf69a35c3fe38f
Abhishek Parashar <aparashar103@gmail.com>
2024-12-07 01:37:44 +0530

    fibonacci dp problem solved

---
+++ dp/Fibonacci.java
@@ -0,0 +1,27 @@
+package dp;
+
+import java.util.Arrays;
+
+class Solution {
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        int ans = fibhelper(n, dp);
+        return ans;
+
+    }
+
+    public int fibhelper(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int a = fibhelper(n - 1, dp);
+        int b = fibhelper(n - 2, dp);
+
+        dp[n] = a + b;
+        return a + b;
+    }
+}
